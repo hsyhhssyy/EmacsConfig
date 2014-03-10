de3aa2088eb9b8f8966707a8c2b67778bff02bbe
hsyhhssyy <hsyhhssyy@gmail.com>
2014-03-10 09:50:42 +0000

    First commit from surface Waiting an auto install of el-get and an installation of magit

--- init.el
+++ init.el
@@ -1,22 +1,50 @@
+;; This is EMACS configuration file
+;; To let emacs find this file, in Windows:
+;; Just put .emacs.d (The SVN root folder, rename it please) folder under %emac_dir% and set
+;; HKEY_LOCAL_MACHINE\SOFTWARE\GNU\Emacs\HOME=%emacs_dir%
+;; In linux, put .emacs.d under HOME
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(current-language-environment "Chinese-GB")
  '(inhibit-startup-screen t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-;; To make emacs find this file, in Windows:
-;; Just put .emacs.d folder under %emac_dir% and set
-;; HKEY_LOCAL_MACHINE\SOFTWARE\GNU\Emacs\HOME=%emacs_dir%
-;; In linux, put .emacs.d under HOME
+;;中文与外文字体设置
+;; Setting English Font
+(set-face-attribute
+'default nil :font "Source Code Pro")
+;; Chinese Font
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+(set-fontset-font (frame-parameter nil 'font)
+charset
+(font-spec :family "微软雅黑" :size 14)))
 
-(load "D:\\Text Editor\\emacs\\mode\\lua-mode.el")
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;;----el-get is a plugin that used to install other plugins---
+;; Commonly used command is M-x el-get-install
+(add-to-list 'load-path "~/.emacs.d/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;(load "~/.emacs.d/lua-mode/lua-mode.el")
+;(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+;(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+;(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(load "~/.emacs.d/csharp-mode/csharp-mode.el")
+
+(add-to-list 'load-path "~/.emacs.d/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
+(ac-config-default)
+(load "~/.emacs.d/auto-complete/auto-complete-etags.el")
+(require 'auto-complete-etags)
